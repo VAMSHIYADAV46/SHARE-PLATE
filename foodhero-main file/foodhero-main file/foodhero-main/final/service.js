3f7ebacb7138aa00ce4747ea16b0783c81d31afb
Vamshi Yadav <nanidx46@gmail.com>
2025-11-24 19:54:21 +0530

    Implement user signup functionality

--- foodhero-main file/foodhero-main file/foodhero-main/final/service.js
+++ foodhero-main file/foodhero-main file/foodhero-main/final/service.js
@@ -1,2 +1,34 @@
 const { storeUser, fetchUserByUserName} = require("./database");
 
+
+async function userSignUp(user) {
+
+    try {
+
+        const fetchedUser = await fetchUserByUserName({ userName: user.userName });
+        if (fetchedUser) {
+            return {
+                success: false,
+                message: "User Already Exist With The Name!",
+            };
+        }
+
+        const result = await storeUser(user);
+        if (result.success) {
+            return {
+                success: true,
+                message: "User Created SuccessFully!",
+                id: result.id,
+
+            }
+        } else {
+            return {
+                success: false,
+                message: "User Could Not Be Created!"
+            }
+        }
+    } catch (error) {
+        throw error;
+    }
+
+}
